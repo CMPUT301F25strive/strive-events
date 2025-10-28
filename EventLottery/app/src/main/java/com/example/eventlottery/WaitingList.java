2da1c4516bb368c7b8f6805613a935488b5a8405
Tyson <t.luu9@share.epsb.ca>
2025-10-27 21:58:48 -0600

    Implemented WaitingList and WaitList Controller

---
+++ EventLottery/app/src/main/java/com/example/eventlottery/WaitingList.java
@@ -0,0 +1,49 @@
+package com.example.eventlottery;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This is a class representing a Waiting List object
+ */
+public class WaitingList {
+    private Event event;
+    private List<EntrantProfile> waitList = new ArrayList<>();
+
+    /**
+     * This methods constructs a WaitingList object
+     * @param event: the event that the waiting list is connected
+     * @param waitList: the list of entrants that joined the waiting list of the event
+     */
+    public WaitingList(Event event, List<EntrantProfile> waitList) {
+        this.event = event;
+        this.waitList= waitList;
+    }
+
+    /**
+     * This adds an entrant to the waiting list
+     * @param entrant: the entrant profile that is joining the list
+     */
+    public void joinList(EntrantProfile entrant) {
+        if (!waitList.contains(entrant)){
+            waitList.add(entrant);
+        }
+    }
+
+    /**
+     * This removes an entrant from the waiting list
+     * @param entrant: the entrant profile that is removed from the list
+     */
+    public void leaveList(EntrantProfile entrant) {
+        waitList.remove(entrant);
+    }
+
+    /**
+     * This returns the number of entrants that joined the waiting list
+     * @return waitList.size(): the amount of entrants in the waiting list
+     */
+    public int getEntrantCount() {
+        return waitList.size();
+    }
+
+}
---
+++ EventLottery/app/src/main/java/com/example/eventlottery/WaitingListController.java
@@ -0,0 +1,76 @@
+package com.example.eventlottery;
+
+import java.util.List;
+
+/**
+ * This class handles the joining and leaving of entrants regarding the waiting list
+ * get the number of entrants in the waiting list
+ * displays the map of the location of all the entrants in the waiting list
+ */
+public class WaitingListController {
+    private EventRepository eventRepository;
+    private ProfileRepository profileRepository;
+
+    /**
+     * This is a constructor of WaitingListController with the given Profile and Event Repository
+     * @param eventRepository: the repository used to manage the events
+     * @param profileRepository: the repository used to manage the profiles
+     */
+
+    public WaitingListController(EventRepository eventRepository, ProfileRepository profileRepository) {
+        this.eventRepository = eventRepository;
+        this.profileRepository = profileRepository;
+    }
+
+    /**
+     * This methods adds entrants based on their entrantID to the waiting list to the event specified by eventID
+     * @param eventID: ID of the event
+     * @param entrantID: ID of the entrant
+     */
+    public void joinWaitingList(int eventID, String entrantID) {
+//        Event event = eventRepository.getEvent(eventID);
+//        EntrantProfile entrant = profileRepository.findEntrantById(entrantID);
+//        WaitingList waitingList = event.getWaitingList(); // might have to add the method getWaitingList() in EventRepository since it shows error without it
+//
+//        if (waitingList.getEntrantCount() >= event.getCapacity()) {
+//            throw new IllegalArgumentException(); //change this exception
+//        }
+//
+//        waitingList.addEntrant(entrant); // might have to add method addEntrant() in ProfileRepository
+//        profileRepository.saveEntrant(entrant);
+    }
+
+    /**
+     * This methods removes entrants based on their entrantID from the waiting list to the event specified by eventID
+     * @param eventID: ID of the event
+     * @param entrantID: ID of the entrant
+     */
+    public void leaveWaitingList(int eventID, String entrantID) {
+//        Event event = eventRepository.getEvent(eventID);
+//        EntrantProfile entrant = profileRepository.findEntrantById(entrantID);
+//        WaitingList waitingList = event.getWaitingList(); // might have to add in EventRepository
+//
+//        waitingList.deleteEntrant(entrant);
+    }
+
+    /**
+     * This methods returns the number of entrants that are in the waiting list
+     * @param eventID: ID of the event
+     * @return int: number of entrants in the waiting list
+     */
+    public int countEntrants(int eventID) {
+//        Event event = eventRepository.getEvent(eventID);
+//        return event.getWaitingList().getEntrantCount(); // might have to add in EventRepository
+        return 0;
+    }
+
+    /**
+     * This methods displays the map of the locations of all the entrants in the waiting list
+     * @param eventID: ID of the event
+     */
+    public void showMapView(int eventID) {
+        Event event = eventRepository.getEvent(eventID);
+        //... // will complete after implementation of the GeoService
+    }
+
+}
