844770b842e3b44884fc3579c5c8faf3685cfa47
Tyson <t.luu9@share.epsb.ca>
2025-11-07 02:59:43 -0700

    Started implementing the InvitationService class and implemented the corresponding test

---
+++ EventLottery/app/src/main/java/com/example/eventlottery/model/InvitationService.java
@@ -0,0 +1,77 @@
+package com.example.eventlottery.model;
+
+import com.example.eventlottery.data.EventRepository;
+import com.example.eventlottery.data.ProfileRepository;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class InvitationService {
+    private EventRepository eventRepository;
+
+    private ProfileRepository profileRepository;
+
+    /**
+     * This is a constructor of InvitationService with the given Profile Repository
+     * @param profileRepository: the repository used to manage the profiles
+     */
+    public InvitationService(EventRepository eventRepository, ProfileRepository profileRepository) {
+        this.eventRepository = eventRepository;
+        this.profileRepository = profileRepository;
+    }
+
+    /**
+     * This is a method that takes the list of winners and sends an invitation to them through notifications
+     * @param winners: list of unique ids that are the winners from the waiting list
+     * @param eventId: title of the event tied to the waiting list winners
+     */
+    public void sendInvitations(List<String> winners, String eventId) {
+        for (String deviceId : winners) {
+            Profile winner = profileRepository.findUserById(deviceId);
+            Event event = eventRepository.findEventById(eventId);
+            if (winner != null) {
+                String title = "You've been invited to join " + event.getTitle() + "!";
+                String message = "Congratulations " + winner.getName() + "for winning a spot! Check the app for more details.";
+                // send message to user through notifications
+            } else {
+                throw new NoSuchElementException("Winner doesn't not exist in repository");
+            }
+        }
+    }
+
+    /**
+     * Accepts an invitation for a specific user and event.
+     * This updates the user's status in the repository or database.
+     * @param deviceId: the ID of the user accepting the invitation
+     * @param eventId: the ID of the event tied to the waiting list winners
+     */
+    public void acceptInvitation(String deviceId, String eventId) {
+        Profile profile = profileRepository.findUserById(deviceId);
+        Event event = eventRepository.findEventById(eventId);
+        if (profile != null) {
+            // Status becomes accepted
+            System.out.println(profile.getName() + " has accepted the invitation to " + event.getTitle());
+        } else {
+            throw new IllegalArgumentException("User not found in repository");
+        }
+    }
+
+
+    /**
+     * Declines an invitation for a specific user and event.
+     * This removes or updates the user's invitation status.
+     * @param deviceId: the ID of the user declining the invitation
+     * @param eventId: the ID of the event tied to the waiting list winners
+     */
+    public void declineInvitation(String deviceId, String eventId) {
+        Profile profile = profileRepository.findUserById(deviceId);
+        Event event = eventRepository.findEventById(eventId);
+        if (profile != null) {
+            event.leaveWaitingList(eventId); // Does the entrant leave the waiting list if they decline?
+            // Status becomes cancelled
+            System.out.println(profile.getName() + " has declined the invitation to " + event.getTitle());
+        } else {
+            throw new IllegalArgumentException("User not found in repository");
+        }
+    }
+}
---
+++ EventLottery/app/src/main/java/com/example/eventlottery/model/PushNotificationService.java
@@ -0,0 +1,4 @@
+package com.example.eventlottery.model;
+
+class PushNotificationService{
+}
