da9cd4139c678442f925f2a8648a041811ddc3fa
Jack Rappel <jrappel@ualberta.ca>
2025-11-07 12:46:16 -0700

    Javadocs for Event

--- EventLottery/app/src/main/java/com/example/eventlottery/model/Event.java
+++ EventLottery/app/src/main/java/com/example/eventlottery/model/Event.java
@@ -39,6 +39,19 @@ public class Event implements Serializable {
     @Nullable
     private final String description;
 
+
+    /**
+     * Event constructor
+     * @param id unique device ID
+     * @param title event title
+     * @param organizerName name of organizer
+     * @param startTimeMillis time the event was created
+     * @param venue location of the evetn
+     * @param spotsRemaining how many spots are left in the waiting list if specified
+     * @param status the status of the event ie open, closed, drawn of finalized
+     * @param posterResId unique poster ID
+     * @param description a description of the event
+     */
     public Event(
             @NonNull String id,
             @NonNull String title,
@@ -66,6 +79,7 @@ public class Event implements Serializable {
     }
 
     /**
+     * Gets the unique ID of the event
      * @return unique identifier for this event.
      */
     @NonNull
@@ -74,6 +88,7 @@ public class Event implements Serializable {
     }
 
     /**
+     * Gets the title of the event
      * @return display title.
      */
     @NonNull
@@ -82,6 +97,7 @@ public class Event implements Serializable {
     }
 
     /**
+     * Gets the organizer's name
      * @return organizer display name.
      */
     @NonNull
@@ -90,6 +106,7 @@ public class Event implements Serializable {
     }
 
     /**
+     * Gets the time created in milliseconds
      * @return event start timestamp in utc millis.
      */
     public long getStartTimeMillis() {
@@ -97,6 +114,7 @@ public class Event implements Serializable {
     }
 
     /**
+     * Gets the name of the venue
      * @return venue description
      */
     @NonNull
@@ -105,6 +123,7 @@ public class Event implements Serializable {
     }
 
     /**
+     * Gets the amount of space the event has
      * @return total capacity configured by the organizer
      */
     public int getCapacity() {
@@ -112,6 +131,7 @@ public class Event implements Serializable {
     }
 
     /**
+     * Gets the amount of spots left in the waiting list
      * @return current remaining entries on the waiting list.
      */
     public int getSpotsRemaining() {
@@ -134,33 +154,58 @@ public class Event implements Serializable {
         return posterResId;
     }
 
+    /**
+     * @return description the description of the event
+     */
     @Nullable
     public String getDescription() {
         return description;
     }
 
+    /**
+     * @return waitingList the waiting list of the event
+     */
     public List<String> getWaitingList() {
         return waitingList;
     }
 
+    /**
+     * Sets the waiting list of the event
+     * @param waitingList the waiting list of the event
+     */
     public void setWaitingList(List<String> waitingList) {
         this.waitingList = waitingList;
     }
 
+    /**
+     * adds a user to the waiting list of the event
+     * @param deviceId the device ID of the user to be added to the waiting list
+     */
     public void joinWaitingList(String deviceId) {
         if (!waitingList.contains(deviceId)) {
             waitingList.add(deviceId);
         }
     }
 
+    /**
+     * removes a user from the waiting list of the event
+     * @param deviceId the device ID of the user to leave the waiting list
+     */
     public void leaveWaitingList(String deviceId) {
         waitingList.remove(deviceId);
     }
 
+    /**
+     * @return size: the amount of people in the waiting list of the event
+     */
     public int getWaitingListSize() {
         return waitingList.size();
     }
 
+    /**
+     * @param deviceId the device ID of the user
+     * @return boolean returns true if user is on the waiting list, false otherwise
+     */
     public boolean isOnWaitingList(String deviceId) {
         return attendeesList.contains(deviceId);
     }
